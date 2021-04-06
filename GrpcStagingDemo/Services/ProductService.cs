2c0357df4d075c2c84e1208449bee36ceb77bcee
adx19 <andoxyap@gmail.com>
2021-04-06 11:44:03 +0800

    INIT grpc demo server

---
+++ GrpcStagingDemo/Services/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using GrpcStagingDemo.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcStagingDemo.Services
+{
+    public class AuthorService : Author.AuthorBase
+    {
+        private readonly ILogger<AuthorService> _logger;
+        private List<AuthorResponse> authors;
+
+        public AuthorService(ILogger<AuthorService> logger)
+        {
+            _logger = logger;
+            authors = new List<AuthorResponse>();
+
+            var antonio = new AuthorResponse { Name = "Antonio Gonzalez" };
+            antonio.BooksAuthored.Add(new BookReply { Title = "Much Ado about nothing" });
+            antonio.BooksAuthored.Add(new BookReply { Title = "How to do a split" });
+            authors.Add(antonio);
+
+            var jack = new AuthorResponse { Name = "Jack Olabisi" };
+            jack.BooksAuthored.Add(new BookReply { Title = "Early morning bird" });
+            jack.BooksAuthored.Add(new BookReply { Title = "Fly me to Paris" });
+            authors.Add(jack);
+        }
+
+        public override Task<AuthorResponse> GetAuthor(AuthorRequest request, ServerCallContext context)
+        {
+            var author = authors.FirstOrDefault(x => x.Name == request.Name);
+            return Task.FromResult(author);
+        }
+    }
+}
---
+++ GrpcStagingDemo/Startup.cs
@@ -0,0 +1,60 @@
+﻿using GrpcStagingDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcStagingDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+            }));
+
+            services.AddSingleton<GrpcCrudService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseGrpcWeb();
+            //app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true }); // default web as true for all grpc
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb()
+                        .RequireCors("AllowAll"); ;
+                endpoints.MapGrpcService<AuthorService>();
+                endpoints.MapGrpcService<GrpcCrudService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
